501726d06b643736989d13d7c139d2dc8b471a1f
Jeremias Alves <jeremiasgoncalez@gmail.com>
2022-01-07 22:40:02 -0300

    Implementando a verificacao de xeque

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -15,6 +15,12 @@ namespace xadrez_console
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if (partida.xeque)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("XEQUE!");
+                Console.ResetColor();
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
